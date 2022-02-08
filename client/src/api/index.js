a97eac6e9385d1d5198ecbe5ab72893eee51f3eb
Arun Kumar G <aguduri@wsgc.com>
2022-02-08 10:04:06 -0800

    client and server code changes for deployment

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const url = "http://localhost:5000/posts";
+const url = "https://memories-mern-server-gak.herokuapp.com/posts";
 // api calls
 export const fetchPosts = () => axios.get(url);
 export const createPost = (newPost) => axios.post(url, newPost);
